d6cc03017fda529a69be2810147b44d34dc1a502
syususp <syususp@yandex.ru>
2023-08-01 22:29:47 +0300

    Add burger icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link, useNavigate } from 'react-router-dom';
 import icon from '../../images/icon.svg';
+import menuIcon from '../../images/menu-icon.svg'; // Иконка для бургер-меню
 
 function Header({ isLoggedIn }) {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // состояние бургер-меню
 
   const handleSignup = () => {
     navigate('/signup');
@@ -14,29 +16,59 @@ function Header({ isLoggedIn }) {
     navigate('/signin');
   };
 
+  const toggleMenu = () => { // обработчик клика для бургер-меню
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <nav className="header__wrapper">
         <Link className="header__logo" to="/"></Link>
         {isLoggedIn ? (
-          <div className="header__links-wrapper">
-            <Link to="/movies" className="header__link">
-              Фильмы
-            </Link>
-            <Link to="/saved-movies" className="header__link">
-              Сохраненные фильмы
-            </Link>
-            <Link to="/profile" className="header__link">
-              Аккаунт
-            </Link>
-            <Link to="/profile" className="header__account-icon-wrapper">
-              <img
-                className="header__account-icon"
-                src={icon}
-                alt="Иконка аккаунта"
-              />
-            </Link>
-          </div>
+          <>
+            {isMenuOpen ? (
+              <div className="header__menu"> {/* Бургер-меню */}
+                <Link to="/movies" className="header__link">
+                  Фильмы
+                </Link>
+                <Link to="/saved-movies" className="header__link">
+                  Сохраненные фильмы
+                </Link>
+                <Link to="/profile" className="header__link">
+                  Аккаунт
+                </Link>
+                <Link to="/profile" className="header__account-icon-wrapper">
+                  <img
+                    className="header__account-icon"
+                    src={icon}
+                    alt="Иконка аккаунта"
+                  />
+                </Link>
+              </div>
+            ) : (
+              <div className="header__links-wrapper">
+                <Link to="/movies" className="header__link">
+                  Фильмы
+                </Link>
+                <Link to="/saved-movies" className="header__link">
+                  Сохраненные фильмы
+                </Link>
+                <Link to="/profile" className="header__link">
+                  Аккаунт
+                </Link>
+                <Link to="/profile" className="header__account-icon-wrapper">
+                  <img
+                    className="header__account-icon"
+                    src={icon}
+                    alt="Иконка аккаунта"
+                  />
+                </Link>
+              </div>
+            )}
+            <button className="header__menu-button" onClick={toggleMenu}> {/* Кнопка бургер-меню */}
+              <img src={menuIcon} alt="Menu" />
+            </button>
+          </>
         ) : (
           <div className="header__buttons-wrapper">
             <button
