69e3f1cdf8087e76a9f798e3daf6b761df5cf71a
syususp <syususp@yandex.ru>
2023-08-13 17:28:27 +0300

    Minor fix - delete unused imports

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Movies.css';
 import Footer from '../Footer/Footer';
 import SearchForm from '../SearchForm/SearchForm';
