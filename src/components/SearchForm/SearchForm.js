5b863f5801d935cc07a19813f24965a95d22dc61
syususp <syususp@yandex.ru>
2023-08-19 01:00:31 +0300

    Add non-browser validation searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,12 +5,14 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 function SearchForm({ onQueryChange, onCheckboxChange, isChecked, page }) {
   const storedQuery = localStorage.getItem(`storedQuery${page}`);
   const [query, setQuery] = useState(storedQuery || '');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     onQueryChange(storedQuery);
   }, [storedQuery]);
 
   const handleInputChange = (e) => {
+    setError('');
     setQuery(e.target.value);
     onQueryChange(e.target.value);
     localStorage.setItem(`storedQuery${page}`, e.target.value);
@@ -18,6 +20,14 @@ function SearchForm({ onQueryChange, onCheckboxChange, isChecked, page }) {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+
+    if (!query.trim()) {
+      setError('Нужно ввести ключевое слово');
+      return;
+    }
+
+    setError('');
+
     localStorage.setItem(`storedQuery${page}`, query);
     onQueryChange(query);
   };
@@ -32,7 +42,12 @@ function SearchForm({ onQueryChange, onCheckboxChange, isChecked, page }) {
 
   return (
     <section className="searchform">
-      <form className="searchform__container" onSubmit={handleFormSubmit}>
+      <span className="searchform__error">{error}</span>
+      <form
+        className="searchform__container"
+        onSubmit={handleFormSubmit}
+        noValidate
+      >
         <div className="searchform__wrapper">
           <input
             className="searchform__input"
